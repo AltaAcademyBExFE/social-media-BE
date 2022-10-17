7642d32ecd17ea253062a497ecaf0150de0ffa3e
Gerdo Trada Wijaya <gerdo.tewel@gmail.com>
2022-10-17 22:25:36 +0700

    Initial Commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
